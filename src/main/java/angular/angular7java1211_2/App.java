44844bd829206d20e598f96802eb1dccddc84c4f
Sora <kangsora0101@gmail.com>
2019-02-11 19:47:13 +0800

    initial commit
    init

---
+++ src/main/java/angular/angular7java1211_2/App.java
@@ -0,0 +1,13 @@
+package angular.angular7java1211_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
